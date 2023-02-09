70e3f76d7f1ef75700654cda202afd114cbb08e6
Laayek Ibne Habib <laayek.teamweb@triosco.com>
2023-02-09 13:35:34 +0530

    UPDATE V1.9 09-02-2023
    by Laayek Ibne Habib

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// BACKGROUND OVERLAY
+// BACKGROUND EFFECT
 function update(e) {
   let x = e.clientX || e.touches[0].clientX;
   let y = e.clientY || e.touches[0].clientY;
@@ -10,10 +10,11 @@ document.addEventListener("touchmove", update);
 // END
 
 // MENU
-let button_open = document.querySelector(".button-open");
-let button_close = document.querySelector(".button-close");
-let nav = document.querySelector(".nav");
-document.querySelector(".burger-menu").addEventListener("click", () => {
+function toggleNav() {
+  const button_open = document.getElementById("button-open");
+  const button_close = document.getElementById("button-close");
+  const nav = document.querySelector(".nav");
+
   if (nav.style.display === "none") {
     nav.style.display = "block";
     button_open.style.display = "none";
@@ -23,12 +24,18 @@ document.querySelector(".burger-menu").addEventListener("click", () => {
     button_open.style.display = "block";
     button_close.style.display = "none";
   }
-});
+}
+
+document.getElementById("burger-menu").addEventListener("click", toggleNav);
 // END
 
 // SECTION 1
-const sectionOne = document.querySelector(".section-1");
-const observerOne = new IntersectionObserver(
+const button_open = document.getElementById("button-open");
+const button_close = document.getElementById("button-close");
+const nav = document.querySelector(".nav");
+const section1 = document.querySelector(".section-1");
+
+const observer1 = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
@@ -50,216 +57,71 @@ const observerOne = new IntersectionObserver(
   },
   { threshold: 1 }
 );
-observerOne.observe(sectionOne);
+
+observer1.observe(section1);
 // END
 
-// SECTION 3
-const section3 = document.querySelector(".section-3");
-const observer3 = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector(".section-title").innerHTML = " | OUR STORY";
-      } else {
-        document.querySelector(".section-title").innerHTML = "";
-      }
+// SECTION TITLES
+const sections = [
+  { class: ".section-3", title: " | OUR STORY" },
+  { class: ".section-4", title: " | CREATE" },
+  { class: ".section-5", title: " | CONNECT" },
+  { class: ".section-6", title: " | TRANSCEND" },
+  { class: ".section-7", title: " | ABOUT OUR SERVICES" },
+  { class: ".section-8", title: " | BRANDS WE HAVE WORKED WITH" },
+  { class: ".section-9", title: " | THE TEAM" },
+  { class: ".section-10", title: " | OUR BLOGS" },
+  { class: ".section-11", title: " | CONTACT US" },
+];
+
+sections.forEach((section, index) => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          document.querySelector(".section-title").innerHTML = section.title;
+        } else {
+          document.querySelector(".section-title").innerHTML = "";
+        }
+      });
+    },
+    { threshold: 1 }
+  );
+  observer.observe(document.querySelector(section.class));
+});
+// END
+
+// HOVER TO SHOW PREVIEW ON SERIVICE PAGE
+const togglePreview = (items, previews) => {
+  for (let i = 0; i < items.length; i++) {
+    const item = document.querySelector(items[i]);
+    const preview = document.querySelector(previews[i]);
+
+    item.addEventListener("mouseover", () => {
+      preview.style.display = "block";
     });
-  },
-  { threshold: 1 }
-);
-observer3.observe(section3);
-// END
 
-// SECTION 4
-const section4 = document.querySelector(".section-4");
-const observer4 = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector(".section-title").innerHTML = " | CREATE";
-      } else {
-        document.querySelector(".section-title").innerHTML = "";
-      }
+    item.addEventListener("mouseleave", () => {
+      preview.style.display = "none";
     });
-  },
-  { threshold: 1 }
+  }
+};
+
+togglePreview(
+  [".item-1", ".item-2", ".item-3"],
+  [".preview-1", ".preview-2", ".preview-3"]
 );
-observer4.observe(section4);
+togglePreview(
+  [".item-1B", ".item-2B", ".item-3B"],
+  [".preview-1B", ".preview-2B", ".preview-3B"]
+);
+togglePreview(
+  [".item-1C", ".item-2C", ".item-3C"],
+  [".preview-1C", ".preview-2C", ".preview-3C"]
+);
 // END
 
-// SECTION 5
-const section5 = document.querySelector(".section-5");
-const observer5 = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector(".section-title").innerHTML = " | CONNECT";
-      } else {
-        document.querySelector(".section-title").innerHTML = "";
-      }
-    });
-  },
-  { threshold: 1 }
-);
-observer5.observe(section5);
-// END
-
-// SECTION 6
-const section6 = document.querySelector(".section-6");
-const observer6 = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector(".section-title").innerHTML = " | TRANSCEND";
-      } else {
-        document.querySelector(".section-title").innerHTML = "";
-      }
-    });
-  },
-  { threshold: 1 }
-);
-observer6.observe(section6);
-// END
-
-// SECTION 7
-const section7 = document.querySelector(".section-7");
-const observer7 = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector(".section-title").innerHTML = " | OUR BLOGS";
-      } else {
-        document.querySelector(".section-title").innerHTML = "";
-      }
-    });
-  },
-  { threshold: 1 }
-);
-observer7.observe(section7);
-// END
-
-// SECTION 8
-const section8 = document.querySelector(".section-8");
-const observer8 = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector(".section-title").innerHTML = " | THE TEAM";
-      } else {
-        document.querySelector(".section-title").innerHTML = "";
-      }
-    });
-  },
-  { threshold: 1 }
-);
-observer8.observe(section8);
-// END
-
-// SECTION 4 CREATE
-const item_1 = document.querySelector(".item-1");
-const item_2 = document.querySelector(".item-2");
-const item_3 = document.querySelector(".item-3");
-const preview_1 = document.querySelector(".preview-1");
-const preview_2 = document.querySelector(".preview-2");
-const preview_3 = document.querySelector(".preview-3");
-
-item_1.addEventListener("mouseover", () => {
-  preview_1.style.display = "block";
-});
-
-item_1.addEventListener("mouseover", () => {
-  preview_1.style.display = "block";
-});
-
-item_1.addEventListener("mouseleave", () => {
-  preview_1.style.display = "none";
-});
-//
-item_2.addEventListener("mouseover", () => {
-  preview_2.style.display = "block";
-});
-
-item_2.addEventListener("mouseleave", () => {
-  preview_2.style.display = "none";
-});
-//
-item_3.addEventListener("mouseover", () => {
-  preview_3.style.display = "block";
-});
-
-item_3.addEventListener("mouseleave", () => {
-  preview_3.style.display = "none";
-});
-//
-
-// SECTION 5 CONNECT
-const item_1B = document.querySelector(".item-1B");
-const item_2B = document.querySelector(".item-2B");
-const item_3B = document.querySelector(".item-3B");
-const preview_1B = document.querySelector(".preview-1B");
-const preview_2B = document.querySelector(".preview-2B");
-const preview_3B = document.querySelector(".preview-3B");
-
-item_1B.addEventListener("mouseover", () => {
-  preview_1B.style.display = "block";
-});
-
-item_1B.addEventListener("mouseleave", () => {
-  preview_1B.style.display = "none";
-});
-//
-item_2B.addEventListener("mouseover", () => {
-  preview_2B.style.display = "block";
-});
-
-item_2B.addEventListener("mouseleave", () => {
-  preview_2B.style.display = "none";
-});
-//
-item_3B.addEventListener("mouseover", () => {
-  preview_3B.style.display = "block";
-});
-
-item_3B.addEventListener("mouseleave", () => {
-  preview_3B.style.display = "none";
-});
-//
-
-// SECTION 6 TRANSCEND
-const item_1C = document.querySelector(".item-1C");
-const item_2C = document.querySelector(".item-2C");
-const item_3C = document.querySelector(".item-3C");
-const preview_1C = document.querySelector(".preview-1C");
-const preview_2C = document.querySelector(".preview-2C");
-const preview_3C = document.querySelector(".preview-3C");
-
-item_1C.addEventListener("mouseover", () => {
-  preview_1C.style.display = "block";
-});
-
-item_1C.addEventListener("mouseleave", () => {
-  preview_1C.style.display = "none";
-});
-//
-item_2C.addEventListener("mouseover", () => {
-  preview_2C.style.display = "block";
-});
-
-item_2C.addEventListener("mouseleave", () => {
-  preview_2C.style.display = "none";
-});
-//
-item_3C.addEventListener("mouseover", () => {
-  preview_3C.style.display = "block";
-});
-
-item_3C.addEventListener("mouseleave", () => {
-  preview_3C.style.display = "none";
-});
-//
-
-//
-
+// REDUCE SCROLL SENSITIVITY
 function wheel(event) {
   var delta = 0;
   if (event.wheelDelta) {
@@ -293,157 +155,55 @@ if (window.addEventListener) {
   window.addEventListener("DOMMouseScroll", wheel, false);
 }
 window.onmousewheel = document.onmousewheel = wheel;
+// END
 
-// click to change img
+// CLICK TO ACTIVATE NAV BUTTONS
+const icons = [
+  ".about-us",
+  ".create",
+  ".connect",
+  ".transcend",
+  ".dot-one",
+  ".dot-two",
+  ".dot-three",
+  ".dot-four",
+  ".call",
+];
 
-document.querySelector(".about-us").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-active.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
+const activeIcons = [
+  "./resources/images/icons/about-us-active.png",
+  "./resources/images/icons/create-active.png",
+  "./resources/images/icons/connect-active.png",
+  "./resources/images/icons/transcend-active.png",
+  "./resources/images/icons/circle-tab-active.png",
+  "./resources/images/icons/circle-tab-active.png",
+  "./resources/images/icons/circle-tab-active.png",
+  "./resources/images/icons/circle-tab-active.png",
+  "./resources/images/icons/contact-active.png",
+];
+
+const inactiveIcons = [
+  "./resources/images/icons/about-us-inactive.png",
+  "./resources/images/icons/create-inactive.png",
+  "./resources/images/icons/connect-inactive.png",
+  "./resources/images/icons/transcend-inactive.png",
+  "./resources/images/icons/circle-tab-inactive.png",
+  "./resources/images/icons/circle-tab-inactive.png",
+  "./resources/images/icons/circle-tab-inactive.png",
+  "./resources/images/icons/circle-tab-inactive.png",
+  "./resources/images/icons/contact-inactive.png",
+];
+
+const updateIcons = (index) => {
+  icons.forEach((icon, i) => {
+    document.querySelector(icon).src =
+      i === index ? activeIcons[i] : inactiveIcons[i];
+  });
+};
+
+icons.forEach((icon, i) => {
+  document.querySelector(icon).addEventListener("click", () => {
+    updateIcons(i);
+  });
 });
-
-document.querySelector(".create").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-active.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
-});
-
-document.querySelector(".connect").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-active.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
-});
-
-document.querySelector(".transcend").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-active.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
-});
-
-document.querySelector(".dot-one").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-active.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
-});
-
-document.querySelector(".dot-two").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-active.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
-});
-
-document.querySelector(".dot-three").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-active.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-inactive.png";
-});
-
-document.querySelector(".call").addEventListener("click", () => {
-  document.querySelector(".about-us").src =
-    "./resources/images/icons/about-us-inactive.png";
-  document.querySelector(".create").src =
-    "./resources/images/icons/create-inactive.png";
-  document.querySelector(".connect").src =
-    "./resources/images/icons/connect-inactive.png";
-  document.querySelector(".transcend").src =
-    "./resources/images/icons/transcend-inactive.png";
-  document.querySelector(".dot-one").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-two").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".dot-three").src =
-    "./resources/images/icons/circle-tab-inactive.png";
-  document.querySelector(".call").src =
-    "./resources/images/icons/contact-active.png";
-});
+// END
