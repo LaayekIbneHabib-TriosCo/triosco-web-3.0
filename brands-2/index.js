f5c12934ee01cf9364a230000babc0b51c5bc90f
Laayek Ibne Habib <laayek.teamweb@triosco.com>
2023-03-17 10:48:32 +0530

    UPDATE V5.1 17-03-2023
    by Laayek Ibne Habib

---
+++ brands-2/index.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+
+document.querySelector(".grid").addEventListener("click", function () {
+  openModal();
+  showSlides(slideIndex);
+});
+
+function openModal() {
+  document.querySelector(".gallery").style.display = "block";
+}
+
+function closeModal() {
+  document.querySelector(".gallery").style.display = "none";
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("img");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
